58c54b8a007437a01902af87837dd1b4c75e84cf
Welington Dias <33883532+Welin7@users.noreply.github.com>
2025-07-06 20:21:47 -0300

    Add service and configuration api key.
    Add service and configuration api key of appsettings file.

--- ProjectOpenAi/Program.cs
+++ ProjectOpenAi/Program.cs
@@ -1,8 +1,17 @@
 using Scalar.AspNetCore;
+using System.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var apiKey = builder.Configuration["OpenAI:ApiKey"];
+
+builder.Services.AddHttpClient("OpenAI", client =>
+{
+    client.BaseAddress = new Uri ("https://api.openai.com/v1/");
+    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
